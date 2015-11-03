61f71506f29a7b2298908d9c3dc07c44973ffdf3
Jesse Herrick <me@jesse.codes>
2015-11-03 16:21:54 -0500

    Add tests for CRUD actions on apples

---
+++ test/ceo/new_apple_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+describe 'apples admin pages' do
+  include TestHelper
+  describe 'new' do
+    before do
+      admin_page '/apples/new'
+    end
+
+    it 'should load' do
+      assert_equal 200, page.status_code
+    end
+
+    it 'should create a new Apple' do
+      fill_in 'apple[name]', with: 'Granny Smith'
+      click_button 'submit'
+      assert_equal 200, page.status_code
+    end
+  end
+end
