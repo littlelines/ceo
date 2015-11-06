4ce1eb1c33b9e18883844f707bc61f5ccbcae6bf
Jesse Herrick <me@jesse.codes>
2015-11-05 22:29:21 -0500

    Load generator into the main engine

--- lib/ceo/engine.rb
+++ lib/ceo/engine.rb
@@ -1,4 +1,7 @@
+require 'generators/admin/admin_generator'
+
 module CEO
   class Engine < Rails::Engine
+    load_generators
   end
 end
--- lib/generators/admin/admin_generator.rb
+++ lib/generators/admin/admin_generator.rb
@@ -1,28 +1,32 @@
+require 'rails/generators'
+
 # Public: Generator for admin page controllers.
 module CEO
-  class AdminGenerator < Rails::Generators::NamedBase
-    source_root File.expand_path('../templates', __FILE__)
+  module Generators
+    class AdminGenerator < Rails::Generators::NamedBase
+      source_root File.expand_path('../templates', __FILE__)
 
-    def create_admin_page_controller
-      @controller = file_name
-      template(
-        'admin_page_controller.rb.erb',
-        "app/controllers/admin/#{@controller.underscore}_controller.rb"
-      )
-    end
-
-    def add_admin_route
-      route "admin_for :#{@controller.underscore}"
-    end
-
-    def add_form_view
-      unless Dir.exist?(Rails.root.join('app/views/admin'))
-        Dir.mkdir(Rails.root.join('app/views/admin'))
+      def create_admin_page_controller
+        @controller = file_name
+        template(
+          'admin_page_controller.rb.erb',
+          "app/controllers/admin/#{@controller.underscore}_controller.rb"
+        )
       end
 
-      create_file "app/views/admin/#{@controller.underscore}.html.erb", <<VIEW
+      def add_admin_route
+        route "admin_for :#{@controller.underscore}"
+      end
+
+      def add_form_view
+        unless Dir.exist?(Rails.root.join('app/views/admin'))
+          Dir.mkdir(Rails.root.join('app/views/admin'))
+        end
+
+        create_file "app/views/admin/#{@controller.underscore}/_form.html.erb", <<VIEW
 <% # "f" is exposed as a form object %>
 VIEW
+      end
     end
   end
 end
