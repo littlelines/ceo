d91d474130bf25e0380f0ecb68e78016ff6ce44c
WendyBeth <wendybeth010@gmail.com>
2016-05-25 15:52:40 -0400

    Adds SVG assets for admin panel

--- lib/ceo.rb
+++ lib/ceo.rb
@@ -3,6 +3,7 @@ require 'ceo/engine'
 require 'ceo/rails/routes'
 require 'ceo/paginator'
 require 'ceo/iterator'
+require 'inline_svg'
 
-module CEO
-end
+module CEO;end
+
--- lib/ceo/engine.rb
+++ lib/ceo/engine.rb
@@ -1,12 +1,5 @@
 module CEO
   class Engine < Rails::Engine
-
-    config.to_prepare do
-      Rails.application.config.assets.precompile += %w(
-        app/assets/stylesheets/ceo/*
-      )
-    end
-
   end
 end
 
